06218a6faf6252a9051e667b636e12f02677c414
ektarathour999-debug <ektarathour999@gmail.com>
2025-12-01 22:25:11 +0530

    Initial files for MakeCode project

---
+++ README.md
@@ -0,0 +1,26 @@
+ 
+
+
+> Open this page at [https://ektarathour999-debug.github.io/lets-see-whose-fingers-are-fast/](https://ektarathour999-debug.github.io/lets-see-whose-fingers-are-fast/)
+
+## Use as Extension
+
+This repository can be added as an **extension** in MakeCode.
+
+* open [https://arcade.makecode.com/](https://arcade.makecode.com/)
+* click on **New Project**
+* click on **Extensions** under the gearwheel menu
+* search for **https://github.com/ektarathour999-debug/lets-see-whose-fingers-are-fast** and import
+
+## Edit this project
+
+To edit this repository in MakeCode.
+
+* open [https://arcade.makecode.com/](https://arcade.makecode.com/)
+* click on **Import** then click on **Import URL**
+* paste **https://github.com/ektarathour999-debug/lets-see-whose-fingers-are-fast** and click import
+
+#### Metadata (used for search, rendering)
+
+* for PXT/arcade
+<script src="https://makecode.com/gh-pages-embed.js"></script><script>makeCodeRender("{{ site.makecode.home_url }}", "{{ site.github.owner_name }}/{{ site.github.repository_name }}");</script>
---
+++ main.ts
@@ -0,0 +1,37 @@
+mp.onButtonEvent(mp.MultiplayerButton.Up, ControllerButtonEvent.Pressed, function (player2) {
+    mp.getPlayerSprite(player2).setImage(assets.image`0`)
+    if (arrow.image.equals(assets.image`0`)) {
+        mp.changePlayerStateBy(player2, MultiplayerState.score, 1)
+    }
+})
+let arrow: Sprite = null
+let arrow_list: Image[] = []
+scene.setBackgroundImage(assets.image`2pbg`)
+bundles.wrap1(function () {
+    mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.One), sprites.create(assets.image`p1`, SpriteKind.Player))
+    mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.One)).setPosition(40, 90)
+})
+bundles.wrap2(function () {
+    mp.setPlayerSprite(mp.playerSelector(mp.PlayerNumber.Two), sprites.create(assets.image`p2`, SpriteKind.Player))
+    mp.getPlayerSprite(mp.playerSelector(mp.PlayerNumber.Two)).setPosition(120, 90)
+})
+bundles.wrap3(function () {
+    arrow_list = [
+    assets.image`0`,
+    assets.image`1`,
+    assets.image`2`,
+    assets.image`3`
+    ]
+    arrow = sprites.create(img`
+        . 
+        `, SpriteKind.Player)
+    arrow.setPosition(80, 30)
+})
+bundles.wrap4(function () {
+    game.splash("Be the quickest to match", "arrow directions to win!")
+    carnival.startCountdownGame(15, carnival.WinTypes.Multi)
+    music.playSoundEffect(music.createSoundEffect(WaveShape.Triangle, 4750, 4783, 255, 0, 449, SoundExpressionEffect.None, InterpolationCurve.Curve), SoundExpressionPlayMode.UntilDone)
+})
+game.onUpdateInterval(500, function () {
+    arrow.setImage(arrow_list._pickRandom())
+})
